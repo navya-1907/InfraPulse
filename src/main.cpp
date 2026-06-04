ca218d06970df65866378b5f22cfb7ff20c56772
Navya Srivastava <navyaaishu11@gmail.com>
2026-06-04 21:39:35 +0530

    Resolved merge conflicts and updated monitoring engine

--- src/main.cpp
+++ src/main.cpp
@@ -5,45 +5,34 @@
 #include "../include/cpu_monitor.h"
 #include "../include/memory_monitor.h"
 #include "../include/disk_monitor.h"
-<<<<<<< HEAD
 #include "../include/process_monitor.h"
 #include "../include/docker_monitor.h"
-#include "../include/dashboard.h"
-=======
->>>>>>> 6cb9bbcce20c7ac5b27c20f74694065e9252f74a
 
 int main() {
 
     CpuMonitor cpu;
     MemoryMonitor memory;
     DiskMonitor disk;
-<<<<<<< HEAD
     ProcessMonitor processMonitor;
     DockerMonitor dockerMonitor;
-    
-=======
->>>>>>> 6cb9bbcce20c7ac5b27c20f74694065e9252f74a
 
     while (true) {
 
         float cpuUsage = cpu.getCpuUsage();
-<<<<<<< HEAD
         float memoryUsage = memory.getMemoryUsage();
         float diskUsage = disk.getDiskUsage();
+
+        auto processes = processMonitor.getProcesses();
+
+        system("clear");
 
         std::cout << "======================================" << std::endl;
         std::cout << "         InfraPulse Monitor           " << std::endl;
         std::cout << "======================================" << std::endl;
-=======
->>>>>>> 6cb9bbcce20c7ac5b27c20f74694065e9252f74a
 
-        std::cout << "CPU Usage: " << cpuUsage << "%" << std::endl;
-
-        std::cout << "Memory Usage: " << memory.getMemoryUsage() << "%" << std::endl;
-
-        std::cout << "Disk Usage: " << disk.getDiskUsage() << "%" << std::endl;
-
-<<<<<<< HEAD
+        std::cout << "CPU Usage      : " << cpuUsage << "%" << std::endl;
+        std::cout << "Memory Usage   : " << memoryUsage << "%" << std::endl;
+        std::cout << "Disk Usage     : " << diskUsage << "%" << std::endl;
         std::cout << "Process Count  : " << processes.size() << std::endl;
 
         std::cout << "======================================" << std::endl;
@@ -71,9 +60,7 @@ int main() {
         dockerMonitor.showContainers();
 
         std::cout << "\nRefreshing in 1 second..." << std::endl;
-=======
-        std::cout << "------------------------" << std::endl;
->>>>>>> 6cb9bbcce20c7ac5b27c20f74694065e9252f74a
+
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
